446508490470f5bcc8386ff5bad74a5aa2b3c46d
Neeraj Choudhary <nc28778@gmail.com>
2025-12-07 16:01:37 +0530

    fix: data intergration issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/sales',
+    baseURL: '/api/sales',
 });
 
 export const getSalesHelper = async (params) => {
